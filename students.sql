02ae5adf061204ac311f07bdda263484d67ba2ea
Saba <sabaverulashvili123@gmail.com>
2026-07-07 14:44:42 +0400

    Add students_git table and data

---
+++ students.sql
@@ -0,0 +1,18 @@
+CREATE TABLE students_git (  
+    id SERIAL PRIMARY KEY, 
+    name VARCHAR(50),
+    age INT
+    );
+    
+INSERT INTO students_git (name,age)
+VALUES 
+   ('Nika', 21),
+   ('Luka', 22),
+   ('Ana', 20),
+   ('Giorgi', 23),
+   ('Tamar', 21),
+   ('Saba', 22),
+   ('Nino', 20),
+   ('Levan', 23),
+   ('Mariam', 21),
+   ('David', 22);
